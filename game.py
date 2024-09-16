b7a98fe1825640f34900348cf99e3261c3b25472
Hila-eng <hilasheto1@gmail.com>
2024-09-16 16:41:09 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,44 +1,56 @@
-import pygame
+import pygame as py
 import time
 import random
+import json
 
-pygame.init()
+py.init()
 
 board = [[0 for x in range(50)] for row in range(25)]
 
-for i in board:
-    print(i)
 
 board[0][0] = 1
 cols, rows = 50, 25
 cell_size = 25
 window_size = (cols * cell_size, rows * cell_size)
-window = pygame.display.set_mode(window_size)
-pygame.display.set_caption("Flag game")
+window = py.display.set_mode(window_size)
+py.display.set_caption("Flag game")
 
-character = pygame.image.load("C:\\Users\\jbt\\Downloads\\lethl.png")
-character = pygame.transform.scale(character, (cell_size * 2, cell_size * 4))
+character = py.image.load("C:\\Users\\jbt\\Downloads\\lethl.png")
+character = py.transform.scale(character, (cell_size * 2, cell_size * 4))
 
-flag = pygame.image.load("C:\\Users\\jbt\\Downloads\\flag.png")
-flag = pygame.transform.scale(flag, (50, 50))
+flag = py.image.load("C:\\Users\\jbt\\Downloads\\flag_0.jpg")
+flag = py.transform.scale(flag, (100, 90))
 
-grss = pygame.image.load("C:\\Users\\jbt\\Downloads\\grass.png")
-grss = pygame.transform.scale(grss, (50, 50))
+grss = py.image.load("C:\\Users\\jbt\\Downloads\\grass.png")
+grss = py.transform.scale(grss, (50, 50))
 
-bomb = pygame.image.load("C:\\Users\\jbt\\Downloads\\bomb.png")
-bomb = pygame.transform.scale(bomb, (50, 50))
+bomb = py.image.load("C:\\Users\\jbt\\Downloads\\bomb.png")
+bomb = py.transform.scale(bomb, (50, 50))
 
 x, y = 0, 0
 
-t, b = 1200, 570
+t, b = 1150, 550
 
 l, g = 0, 0
 
 speed = 1
 
+
+font = py.font.Font('freesansbold.ttf', 40)
+
+lose_text = font.render('You Lose', True, 'black')
+lose_textRect = lose_text.get_rect()
+win_text = font.render('You Won', True, 'black')
+win_textRect = win_text.get_rect()
+lose_textRect.center = (window_size[0] // 2, window_size[1] // 2)
+win_textRect.center = (window_size[0] // 2, window_size[1] // 2)
+display_surface = py.display.set_mode((window_size[0], window_size[1]))
+
+KEYS = [py.K_1, py.K_2, py.K_3, py.K_4, py.K_5, py.K_6, py.K_7, py.K_8, py.K_9]
+
+game_start = 0
 grs_amount = 20
 grss_graft = []
-
 for i in range(grs_amount):
     grss_x = random.randint(0, cols - 1) * cell_size
     grss_y = random.randint(0, rows - 1) * cell_size
@@ -53,14 +65,6 @@ for i in range(bomb_amount):
         bombs_apper.append((bomb_x, bomb_y))
         board[bomb_x][bomb_y] = 3
 
-    # if board[bomb_x][bomb_y] == board[bomb_x][bomb_y] :
-    #     print("game over!")
-    #     pygame.quit()
-
-    # if board[bomb_x][bomb_y] == 1 in bombs_apper[i]:
-    #     print("game over!")
-    #     pygame.quit()
-
 board[22][47] = 4
 
 grid = False
@@ -73,64 +77,90 @@ for i in range(21,25):
     for j in range(46,50):
         flag_list.append((i,j))
 
+
 while running:
     window.fill('light green')
-
-    # for i in range():
-    #     if board[bomb_x][bomb_y] == x and bomb_y == y:
-    #         print("game over!")
-    #         pygame.quit()
 
     if grid:
         if time.time() - grid_start_time > grid_duration:
             grid = False
 
         window.fill((68, 69, 71))
-        # window.blit(character, (x, y))
+        window.blit(character, (x * 25, y * 25))
         window.blit(flag, (t, b))
 
         for row in range(rows):
-            pygame.draw.line(window, 'light green', (0, row * cell_size), (window_size[0], row * cell_size))
+            py.draw.line(window, 'light green', (0, row * cell_size), (window_size[0], row * cell_size))
         for col in range(cols):
-            pygame.draw.line(window, 'light green', (col * cell_size, 0), (col * cell_size, window_size[1]))
+            py.draw.line(window, 'light green', (col * cell_size, 0), (col * cell_size, window_size[1]))
 
         for i in bombs_apper:
             window.blit(bomb, (i[1] * cell_size, i[0] * cell_size))
 
     else:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                exit()
+
+            for i in KEYS:
+                if event.type == py.KEYDOWN:
+                    if event.key == i:
+                        game_start = py.time.get_ticks()
+                if event.type == py.KEYUP:
+                    if event.key == i:
+                        game_end = py.time.get_ticks()
+                        if game_end - game_start < 1000:
+                            data = {
+                                'player_pos': (x,y),
+                                'bomb': bombs_apper,
+                                'grass': grss_graft,
+                                'board': board
+                            }
+                            file_data = open('data.txt', 'w')
+                            json.dump(data,file_data)
+                            file_data.close()
+
+                        else:
+                            file_data = open('data.txt', 'r')
+                            file = json.load(file_data)
+                            bombs_apper = file['bomb']
+                            x, y = file['player_pos'][0], file['player_pos'][1]
+                            grss_graft = file['grass']
+                            board = file['board']
+                            file_data.close()
+
+            if event.type == py.KEYDOWN:
+                if event.key == py.K_RETURN:
                     grid = True
                     grid_start_time = time.time()
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT] and x > 0:
+
+            keys = py.key.get_pressed()
+            if keys[py.K_LEFT] and x > 0:
                 board[y][x] = 0
                 x -= speed
                 board[y][x] = 1
                 for i in board:
                     print(i)
-            if keys[pygame.K_RIGHT] and x < 48:
+            if keys[py.K_RIGHT] and x < 48:
                 board[y][x] = 0
                 x += speed
                 board[y][x] = 1
                 for i in board:
                     print(i)
-            if keys[pygame.K_UP] and y > 0:
+            if keys[py.K_UP] and y > 0:
                 board[y][x] = 0
                 y -= speed
                 board[y][x] = 1
                 for i in board:
                     print(i)
-            if keys[pygame.K_DOWN] and y < 21:
+            if keys[py.K_DOWN] and y < 21:
                 board[y][x] = 0
                 y += speed
                 board[y][x] = 1
                 for i in board:
                     print(i)
             print(x, y)
+
 
         for i in grss_graft:
             window.blit(grss, (i[0], i[1]))
@@ -139,41 +169,24 @@ while running:
         for j in (x, x + 2):
             player_pos.append((y + 3, j))
 
-        for i in player_pos:
-            if i in bombs_apper:
-                window.fill('red')
-                pygame.quit()
+            for i in player_pos:
+                if i in bombs_apper:
+                    display_surface.fill('white')
+                    display_surface.blit(lose_text, lose_textRect)
+                    py.display.flip()
+                    time.sleep(2)
+                    quit()
 
-            if i in flag_list:
-                window.fill('blue')
-
-            #     font = pygame.font.SysFont('david', 50)
-            #     surface = font.render('you lose', True, "black")
-            #
-            #     x = surface.get_rect()
-            #
-            #     x.midtop = (window_size[0] / 2, window_size[1] / 4)
-            #
-            #     window.blit(surface, x)
-            #
-            #     pygame.display.flip()
-            #     time.sleep(2)
-            #
-            # pygame.quit()
-
-        # print(player_pos)
-        # print(bombs_apper)
-
-            # if i in flag_list:
-            #     window.fill('blue')
-
-    window.blit(character, (x * 25, y * 25))
-
-    window.blit(flag, (t, b))
-
-    pygame.display.flip()
-
-pygame.quit()
+                if i in flag_list:
+                    window.fill('light blue')
+                    display_surface.blit(win_text, win_textRect)
+                    py.display.flip()
+                    time.sleep(2)
+                    quit()
 
 
+        window.blit(character, (x * 25, y * 25))
+        window.blit(flag, (t, b))
 
+    py.display.flip()
+py.quit()
